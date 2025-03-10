03e0a129f95f94a993738790a186f5edd9bf85a3
Silva-NK <kiagonsuzanne@gmail.com>
2025-03-10 23:55:23 +0300

    Adds four arithmetic functions to index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 =  2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+
+const random = Math.floor(Math.random() * 100) + 1; //Math.random - generates integer. Math.floor -rounds down. (*100)+1 - Ensure the generated number is over 0 & 1.
+
+
+
+const num3 = 32;
+const num4 = 7;
+
+const mod = num3 % num4;
+
+
+
+const max = Math.max(6, 13, 10, 18, 20, 2, 7, 15);
